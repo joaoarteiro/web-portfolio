be5ebb345e6fd290bd55e924d83d35e45a34003c
João <joaoarteiro.jb@gmail.com>
2020-09-29 13:59:50 +0200

    containers added

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -5,12 +5,35 @@ import '../../App.css'
 function Experience() {
   return (
     <div className="App">
-    <div className="experience-section" id="experience">
-      <div className="title">
-        <h1>EXPERIENCE</h1>
-          <div className="text"><h5>Android Developer</h5></div>
+      <div className="experience-section" id="experience">
+        <div className="title">
+          <h1>EXPERIENCE</h1>
         </div>
-    </div >
+        <div className="experience-container">
+            <div className="color-text">Freelancer</div>
+            <div className="strong-title">Web Developer
+               <div className="date-interval">Sep 2020 - Present</div>
+            </div>
+
+            <div className="description">aisjdilajsdiasjdajsodijasiodjaasdihasiodhasiodhoaihsdioauksdhaukshdasdaihsdoia<br/>
+                 aisdioahsiodhasiohdioashdioashdioahdioashdaishdaoishdaosihdoiashdoiashdioashdoianqww
+            </div>
+          
+        </div>
+
+        <div className="experience-container">
+          
+            <div className="color-text">ITSector</div>
+            <div className="strong-title">Android Developer
+                <div className="date-interval">Feb 2020 - Jul 2020</div>
+            </div>
+
+            <div className="description">aisjdilajsdiasjdajsodijasiodjaasdihasiodhasiodhoaihsdioauksdhaukshdasdaihsdoia<br/>
+                 aisdioahsiodhasiohdioashdioashdioahdioashdaishdaoishdaosihdoiashdoiashdioashdoianqww
+            </div>
+        
+        </div>
+      </div >
   </div>
   );
 }
