bb75e556ca0a97b611dc4b4953c35fa129015130
joao.arteiro <joaoarteiro.jb@gmail.com>
2021-03-23 13:28:37 +0100

    removed facebook link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,7 +8,6 @@ function Contact() {
         <div className="contact-icons">
           <a href="https://www.linkedin.com/in/joao-arteiro/" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin"> </a>  
           <a href="https://github.com/joaoarteiro" target="_blank" rel="noopener noreferrer" className="fa fa-github"> </a> 
-          <a href="https://www.facebook.com/joao.bernardo.566/" target="_blank" rel="noopener noreferrer" className="fa fa-facebook"> </a>  
           <a href="mailto:joaoarteiro@gmail.com" className="fa fa-envelope"> </a>   
         </div>
       </div >
